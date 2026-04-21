3079e3a3a3a42f77e75dead0177f7f997e1ab89d
aniezesidney6-source <aniezesidney6@gmail.com>
2026-04-21 18:07:16 +0100

    Delete prive-booking (2)/prive-booking directory

--- prive-booking (2)/prive-booking/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import PriveBooking from './PriveBooking'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <PriveBooking />
-  </StrictMode>
-)
